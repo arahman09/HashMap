dd2f931d6bbd74064cb50ce17299c807c52b9c2c
Arifur Rahman <sajolusa84@gmail.com>
2020-04-01 16:40:06 -0400

    first Commit

--- HashMap/TestHashMap.java
+++ HashMap/TestHashMap.java
@@ -1,27 +1,29 @@
 package HashMap;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
 
 public class TestHashMap {
 
     public static void main(String[] args) {
-        Map<String, String > mapObject = new HashMap<String, String>();
+    HashMap<String, String> mapObject = new LinkedHashMap<>();
 
         mapObject.put ("BrandName: ", "Audi");
         mapObject.put("HorsePower: ", "340");
         mapObject.put ("Type: ", "4 Wheel");
         mapObject.put ("Price: ", "$70,000");
-        mapObject.put ("Origin: ", "Germeny");
+        mapObject.put ("Origin: ", "Germany");
 
-        System.out.println("Elements of Map: ");
-        System.out.println(mapObject);
+        System.out.println("Elements of Map:");
+        System.out.println();
 
+        for (HashMap.Entry carModel:mapObject.entrySet())
+        System.out.println(carModel.getKey()+ " " +carModel.getValue());
 
 
     }
-
-
 
 
 }
